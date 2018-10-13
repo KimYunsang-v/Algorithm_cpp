c618364fc977de4de7a8d84a0b5aed4d9e5dbd05
kimYSang <qwdbstkd@naver.com>
2018-10-13 20:32:55 +0900

    백준 2193

--- 2193.cpp
+++ 2193.cpp
@@ -4,22 +4,12 @@ using namespace std;
 int main(){
 	int n;
 	cin >> n;
-	int j = 1;
 
-	int *a = new int[n+1];
-	a[0] = 1;
+	long long *a = new long long[n+1];
+	a[0] = 0;
 	a[1] = 1;
-	a[2] = 1;
-	a[3] = 2;
-	if(n <= 2)
-		cout << 1;
-	else if( n == 3)
-		cout << a[3];
-	else{
-		for(int i = 4; i <= n; i++){
-			a[i] = a[i-1] + j;
-			j++;
-		}
-		cout << a[n];
+	for(int i = 2; i <= n; i++){
+		a[i] = a[i-1] + a[i-2];
 	}
+	cout << a[n];
 }
